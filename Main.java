67aaf7cf3585b8ac4824ec23ac231b7a6459959e
Aayam Neupane <aayamneupane@Mac-4508.lan>
2026-09-21 22:49:23 -0400

    tic-tac-toe board setup

--- Main.java
+++ Main.java
@@ -1,8 +1,18 @@
 // This is the Main file for Java Tic-Tac-Toe Game.
 
-
 public class Main{
     public static void main(String[] args) {
-        System.out.println("This is the main java game");
+        char[][] board = {
+            {' ', ' ', ' '}, 
+            {' ', ' ', ' '}, 
+            {' ', ' ', ' '}
+        };
+
+        System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+        System.out.println("-+-+-");
+        System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+        System.out.println("-+-+-");
+        System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+        // System.out.println("-+-+-"); not needed
     }
 }
