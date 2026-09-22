05a2a1564e3f290eb59f5ef039d6978dfde8cd5b
Aayam Neupane <aayamneupane@Mac-4508.lan>
2026-09-21 23:03:57 -0400

    tic-tac-toe input handled for first iteration

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 // This is the Main file for Java Tic-Tac-Toe Game.
 
 public class Main{
@@ -8,15 +11,67 @@ public class Main{
             {' ', ' ', ' '}
         };
 
-        generateBoard(board);
-        // System.out.println("-+-+-"); not needed
-    }
+        updateBoard(board);
 
-    private static void generateBoard(char[][] board) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Where would you like to play? (1-9)");
+        String userInput = scanner.nextLine();
+
+        System.out.println(userInput);
+
+        switch (userInput) {
+            case "1":
+                board[0][0] = 'X';
+                break;
+            
+            case "2":
+                board[0][1] = 'X';
+                break;
+            
+            case "3":
+                board[0][2] = 'X';
+                break;
+
+            case "4":
+                board[1][0] = 'X';
+                break;
+
+            case "5":
+                board[1][1] = 'X';
+                break;
+
+            case "6":
+                board[1][2] = 'X';
+                break;
+
+            case "7":
+                board[2][0] = 'X';
+                break;
+
+            case "8":
+                board[2][1] = 'X';
+                break;
+
+            case "9":
+                board[2][2] = 'X';
+                break;
+            default:
+                System.out.println("I am sad");
+        }
+
+        updateBoard(board);
+
+
+        
+
+    }
+    
+    private static void updateBoard(char[][] board) {
         System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
         System.out.println("-+-+-");
         System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
         System.out.println("-+-+-");
         System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+        // System.out.println("-+-+-"); not needed
     }
 }
